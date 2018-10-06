c1ceac9701d39aba7951e89a293369002cda81cd
SaintBallista <jmaniquehenz@gmail.com>
2018-10-05 21:58:27 -0300

    fixing main file

--- generallib.py
+++ generallib.py
@@ -6,26 +6,20 @@ class Attribute:
 		self.element_list = element_list
 		self.possib_number = 0  # numero de valores que o atributo pode assumir
 
-
 class Dataset:
 	def __init__(self):
 		self.attlist = [] #lista de "Attribute"
 		self.attnum = 0
 		self.datasize = 0
 
-
-	
-
 def callexit():
     print('')
     exit()
 
-
-
 def readdataset(datapath):
-    
+
 	with open(datapath, 'rb') as csvfile:
-    	
+
 		dataset = Dataset()
 
 		csvline = csv.reader(csvfile, delimiter=';')
@@ -33,20 +27,20 @@ def readdataset(datapath):
 		i = 0
 
 		for row in csvline:
-			
+
 			if(i==0): # primeira linha
-				
+
 				dataset.attnum = len(row)
 
 				for j in range(dataset.attnum):
 					dataset.attlist.append(Attribute(row[j], [])) # instancia cada atributo
 
 				i+=1
-				
+
 			else:
 				for j in range(dataset.attnum):
 					dataset.attlist[j].element_list.append(row[j]) # enche a lista de atributos
-			
+
 	for i in range(dataset.attnum):
 		dataset.attlist[i].possib_number = len(set(dataset.attlist[i].element_list))
 	#	print(dataset.attlist[i].possib_number)
--- machinelearning_1.py
+++ machinelearning_1.py
@@ -1,24 +1,25 @@
 import sys
 from generallib import *
+import random
 
-#
-#numberofargs = 4+1
-#if len(sys.argv) < numberofargs:
-#    print('arguments are: 1.randomseed 2.ntreeparameter 3.datasetpath 4.outputpath')
-#    callexit()
-#print('thats correct')
-# random.seed(sys.argv[1])
-#ntreeparameter = sys.argv[2]
-#try:
-    #dataset = readdataset(sys.argv[3])
-dataset = readdataset("dadosBenchmark_validacaoAlgoritmoAD.csv") 
+numberofargs = 4+1
+if len(sys.argv) < numberofargs:
+    print('arguments are: 1.randomseed 2.ntreeparameter 3.datasetpath 4.outputpath')
+    callexit()
+if (sys.argv[3]=='-'):
+    datasetpath = 'dadosBenchmark_validacaoAlgoritmoAD.csv'
+else:
+    datasetpath = sys.argv[3]
+random.seed(sys.argv[1])
+ntreeparameter = sys.argv[2]
+try:
+    dataset = readdataset(datasetpath)
+    print('this is the dataset:',dataset)
+    # tree = create_tree(dataset)
 
-# tree = create_tree(dataset)
-
-#except:
-	#print(sys.argv[3])
-	#print('something wrong with the dataset path!')
-	#callexit()
+except Exception as e:
+	print('Error!\n',e)
+	callexit()
 
 
 
