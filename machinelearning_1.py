1de626e3030f7420a000b19bd63c218a111abc47
Julio Cesar de Azeredo <jcdazeredo@gmail.com>
2018-10-09 14:48:30 -0300

    Bugs fixed

--- machinelearning_1.py
+++ machinelearning_1.py
@@ -41,14 +41,34 @@ dataset_original = pd.read_csv("dadosBenchmark_validacaoAlgoritmoAD.csv", sep = 
 attribute_list = np.array(dataset_original.iloc[:,:-1].columns.values)
 
 # Cria floresta
-floresta = Random_Forest(y_column, dataset_original, attribute_list, 13)
+floresta = Random_Forest(y_column, dataset_original, attribute_list, 1)
 floresta.fit()
-y_pred = floresta.classify(dataset_original)
 
-cm = confusion_matrix(y_pred, dataset_original.iloc[:, -1])
+y_pred, mode = floresta.classify(dataset_original.iloc[:, :-1])
+
+y_actual = dataset_original.iloc[:, y_column]
+y_pred = np.reshape(y_pred, (y_actual.shape))
+
+# Resultado do classificador
+classifier_result = y_pred == y_actual
+accuracy = np.sum(classifier_result) / dataset_original.shape[0]
+cm = confusion_matrix(y_actual, y_pred)
 
 
 
+
+
+## Feature Scaling
+#from sklearn.preprocessing import StandardScaler
+#sc = StandardScaler()
+#
+#string_column = 3
+#dataset_original.iloc[:, string_column] = sc.fit_transform(dataset_original.iloc[:, string_column:string_column+1])
+#dataset_original.iloc[:, string_column], bins = pd.qcut(dataset_original.iloc[:, string_column], 5, labels=False, retbins = True)
+#
+#dataset2 = pd.read_csv("/home/jcdazeredo/Dropbox/Faculdade/UFRGS/Indo/Aprendizado/dataset2.csv")
+#dataset2.iloc[:, string_column] = sc.transform(dataset2.iloc[:, string_column:string_column+1])
+#dataset2.iloc[:, string_column] = pd.qcut(dataset2.iloc[:, string_column], 5, labels=False)
 
 # =============================================================================
 # Usando árvore simples
--- random_forest.py
+++ random_forest.py
@@ -19,7 +19,6 @@ class Random_Forest(object):
         self.attribute_list = attribute_list
         self.bootstrap_list = None
         self.tree_list = [None]*num_trees
-        self.cm = None
         self.accuracy = None
         
     def fit(self):
@@ -28,14 +27,25 @@ class Random_Forest(object):
         for i in range(self.num_trees):
             self.tree_list[i] = Tree(self.y_column, self.bootstrap_list[i].training_set, self.attribute_list, True)
             self.tree_list[i].fit()
+            
+        self.validation()
+        print("Mean Accuracy: " + str(self.accuracy))
 #            self.tree_list[i].printree()
         
     def create_bootstrap(self):
         self.bootstrap_list = bs.create_bootstrap_list(self.dataset, self.num_trees, 0.7)
   
     def validation(self):
-        pass
-
+        accuracy = 0
+        # Faz a predição de todas as árvores para o mesmo dataset
+        for i in range(self.num_trees):
+            y_pred = self.tree_list[i].classify(self.bootstrap_list[i].test_set)
+            y_actual = (self.bootstrap_list[i].test_set).iloc[:, self.y_column]
+            y_pred = np.reshape(y_pred, (y_actual.shape))
+            
+            accuracy += np.sum(y_pred == y_actual) / y_actual.shape[0]
+            
+            self.accuracy = accuracy/self.num_trees
             
     def classify(self, dataset):
         predictions = [None]*self.num_trees
@@ -49,4 +59,4 @@ class Random_Forest(object):
         # Verifica qual teve mais votação
         mode = stats.mode(predictions)
         y_pred = mode[0].transpose()   
-        return y_pred
+        return y_pred, mode
