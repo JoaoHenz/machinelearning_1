324bc342e277e3ed611dcf5ba9e6f9b815d30a9f
Julio Cesar de Azeredo <jcdazeredo@gmail.com>
2018-10-09 22:24:25 -0300

    foi

--- machinelearning_1.py
+++ machinelearning_1.py
@@ -11,7 +11,12 @@ import generallib as gl
 from sklearn.metrics import confusion_matrix
 from random_forest import Random_Forest
 
-# Coluna a ser predita (-1 == ultima)
+
+
+# =============================================================================
+# Pre - Processing
+# =============================================================================
+# Coluna a ser predita
 y_column = 0
 
 header = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"]
@@ -30,8 +35,9 @@ attribute_list = np.delete(attribute_list, y_column)
 
 
 
-
-# Cria floresta
+# =============================================================================
+# Random Forest
+# =============================================================================
 floresta = Random_Forest(y_column, dataset_original.iloc[0:100, :], attribute_list, 10)
 floresta.fit()
 
@@ -44,3 +50,60 @@ y_pred = np.reshape(y_pred, (y_actual.shape))
 classifier_result = y_pred == y_actual
 accuracy = np.sum(classifier_result) / dataset_original.shape[0]
 cm = confusion_matrix(y_actual, y_pred)
+
+
+
+
+
+
+
+
+
+
+# =============================================================================
+# Pre - Processing
+# =============================================================================
+# Coluna a ser predita
+y_column = 0
+
+# Carregando CSV
+dataset_original = pd.read_csv("dadosBenchmark_validacaoAlgoritmoAD.csv", sep = ";")
+
+header = dataset_original.columns.values
+x = gl.stratified_kcross_validation(dataset_original,3, header[4])
+
+# Lista de atributos do dataset
+attribute_list = np.array(dataset_original.columns.values)
+attribute_list = np.delete(attribute_list, y_column)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- simple_tree.py
+++ simple_tree.py
@@ -118,13 +118,8 @@ class Tree(object):
             att_list = np.delete(att_list, att_index_original)
 
             for value in att_values:
-<<<<<<< HEAD
                 dataset_v = dataset_continuous[dataset_continuous.loc[:, att] == value]
-    
-=======
-                dataset_v = dataset[dataset.loc[:, att] == value]
 
->>>>>>> e170207a295591543b6fcf576f39c76d5552923b
                 if dataset_v.shape[0] == 0: # subset ta vazio
                     new_node1 = Node()
 
@@ -141,7 +136,6 @@ class Tree(object):
                     new_node.children_list.append(pair)
 
         return new_node
-<<<<<<< HEAD
     
     def check_continuous(self, dataset):
         for i  in range(0, dataset.shape[1]):
@@ -160,9 +154,6 @@ class Tree(object):
         
         return dataset
     
-=======
-
->>>>>>> e170207a295591543b6fcf576f39c76d5552923b
     def print_tree(self, tree, tab, inicial):
         if not(inicial):
             tab += "\t"
@@ -228,14 +219,4 @@ class Tree(object):
             if flag == 0:
                 return -1
             return pred
-        
-=======
-            for child in children:
-                if child[0] == self.features_vector.loc[0, node.info]:
-                    x = self.classify_core(child[1])
-                    flag = 1
-
-            if flag == 0:
-                return -1
-            return x
->>>>>>> e170207a295591543b6fcf576f39c76d5552923b
+    
